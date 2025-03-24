7c2bf5d30e9021fbd0dcf5cca123e96ac94ee163
Tehilah Zamanzadeh <tehilah.zaman@gmail.com>
2025-03-24 16:17:12 -0700

    adjusted css and a tags

--- js/index.js
+++ js/index.js
@@ -12,3 +12,17 @@ function changeText() {
 function changeStyle() {
   emailButton.style.width = "200px";
 }
+
+emailButton.addEventListener("contextmenu", (event) => {
+  event.preventDefault;
+
+  const copyText = document.getElementById("emailButton");
+  navigator.clipboard
+    .writeText(copyText.value)
+    .then(() => {
+      alert("Copied: " + copyText.value);
+    })
+    .catch((err) => {
+      console.error("Failed to copy: ", err);
+    });
+});
